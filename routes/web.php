82f1f312899f9fc481220b2eaa27c8feef410ad3
kevin-softlogic <kevin@softlogic.com.gt>
2025-03-27 20:44:47 -0600

    Readme updated

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-/* 
-Route::get('api/matches', [LeagueMatchController::class, 'index']);
-Route::get('api/matches/{id}', [LeagueMatchController::class, 'show']);
-Route::put('api/matches/{id}', [LeagueMatchController::class, 'update']);
-Route::post('api/matches', [LeagueMatchController::class, 'store']);
-Route::delete('api/matches/{id}', [LeagueMatchController::class, 'destroy']); */
